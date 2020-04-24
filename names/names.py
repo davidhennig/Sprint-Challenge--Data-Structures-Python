1929fcfd97fa36d130a278fa13766a8efe5255e8
davidhennig <davidjhennig@gmail.com>
2020-04-24 13:00:05 -0400

    finished

--- names/names.py
+++ names/names.py
@@ -13,14 +13,17 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
+# Was O(n^2), now is O(n)
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    # for name_2 in names_2:
+    if name_1 in names_2:
+        duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,27 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length == self.capacity:
+            if self.current is None:
+                self.current = self.storage.head
+            self.current.value = item
+            if self.current.next:
+                self.current = self.current.next
+            else:
+                self.current = self.storage.head
+        else:
+            self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        current = self.storage.head
+        while current:
+            if current.value is not None:
+                list_buffer_contents.append(current.value)
+            current = current.next
 
         return list_buffer_contents
 
